a640689d7badfbc126378aa1aac9cdf018ab4091
yanniboi <yan@yanniboi.com>
2014-03-11 20:02:45 +0000

    ondeviceready

--- www/js/index.js
+++ www/js/index.js
@@ -1,12 +1,28 @@
-var db;
-
-$('#reposHome').bind('pageinit', function(event) {
-    console.log("Load Start");
-    loadRepos();
-    console.log("Load finish");
-    db = window.openDatabase("repodb","0.1","GitHub Repo Db", 1000);
-    db.transaction(createDb, txError, txSuccess);
-});
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicity call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        var db;
+        console.log("Load Start");
+        loadRepos();
+        console.log("Load finish");
+        db = window.openDatabase("repodb","0.1","GitHub Repo Db", 1000);
+        db.transaction(createDb, txError, txSuccess);
+    }
+};
 
 function createDb(tx) {
     tx.executeSql("DROP TABLE IF EXISTS repos");
